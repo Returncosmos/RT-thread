0d469ce59981f88aed443211b1bd6d649a917e50
Returncosmos <1106290759@qq.com>
2022-06-05 23:36:56 +0800

    重映射串口到kt_kprintf

--- User/board.h
+++ User/board.h
@@ -4,7 +4,9 @@
 #include "stm32h7xx.h"
 
 #include "bsp_led.h"
-
+#include "./led/bsp_led.h"
+#include "./delay/core_delay.h" 
+#include "./usart/bsp_debug_usart.h"
 
 void rt_hw_board_init(void);
 void SysTick_Handler(void);
